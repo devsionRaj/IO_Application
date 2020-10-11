66385565beb5476873593e555f0f06f8cbce268a
Raj Kumar Das <devsionraj@hotmail.com>
2020-10-11 07:54:06 +0530

    Initial commit to the route layer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const EventEmitterService = require('../services/events.service');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.status(200).json({
+    message: 'Your application is up and running!'
+  })
 });
+
+router.get('/checkEventEmitter', (req, res) => {
+  try {
+    EventEmitterService.eventEmition();
+    res.status(200).json({
+      message:'Event emitter executed successfully'
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: 'Event emitter execution failed!',
+      error: error
+    })
+  }
+})
 
 module.exports = router;
---
+++ services/events.service.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events');
+
+const eventEmition = () => {
+    // Initializing event emitter instances
+    var eventEmitter = new EventEmitter();
+
+    // Registering to myEvent
+    eventEmitter.on('myEvent', (msg) => {
+        console.log(msg)
+    })
+
+    // Triggering myEvent
+    eventEmitter.emit('myEvent', "First event");
+}
+
+module.exports={
+    eventEmition
+}
